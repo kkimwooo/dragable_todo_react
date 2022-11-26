fd97f7b21e87c0092afc3eb49a4aab0d629481a1
kkimwooo <kkimwooo@gmail.com>
2022-11-26 22:29:01 +0900

    functional component로 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-export default class App extends Component {
-  state = {
-    todoData: [],
-    value: "",
-  };
+export default function App() {
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
 
-  btnStyle = {
+  const btnStyle = {
     color: "#fff",
     border: "none",
     padding: "5px 9px",
@@ -16,7 +14,7 @@ export default class App extends Component {
     float: "right",
   };
 
-  getStyle = (checked) => {
+  const getStyle = (checked) => {
     return {
       background: "#f4f4f4",
       padding: "10px",
@@ -25,81 +23,73 @@ export default class App extends Component {
     };
   };
 
-  onClickDelte = (id) => {
-    let newTodoData = this.state.todoData.filter((todo) => todo.id !== id);
-    this.setState({ todoData: newTodoData });
+  const onClickDelte = (id) => {
+    let newTodoData = todoData.filter((todo) => todo.id !== id);
+    setTodoData(newTodoData);
   };
 
-  onChange = (e) => {
-    this.setState({ value: e.target.value });
+  const onChange = (e) => {
+    setValue(e.target.value);
   };
 
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     let newTodoData = {
       id: Date.now(),
-      title: this.state.value,
+      title: value,
       checked: false,
     };
-
-    this.setState({
-      todoData: [...this.state.todoData, newTodoData],
-      value: "",
-    });
+    setTodoData((prev) => [...prev, newTodoData]);
+    setValue("");
   };
 
-  onChecked = (id) => {
-    let newTodoData = this.state.todoData.map((todo) => {
+  const onChecked = (id) => {
+    let newTodoData = todoData.map((todo) => {
       if (todo.id === id) {
         todo.checked = !todo.checked;
       }
       return todo;
     });
 
-    this.setState({ todoData: newTodoData });
+    setTodoData(newTodoData);
   };
 
-  render() {
-    return (
-      <div className="container">
-        <div className="todoBlock">
-          <div className="title">
-            <h1>Todo List</h1>
-          </div>
-          <form style={{ display: "flex" }} onSubmit={this.onSubmit}>
-            <input
-              type="text"
-              name="name"
-              style={{ flex: "10", padding: "5px" }}
-              placeholder="할 일을 입력하세요"
-              value={this.state.value}
-              onChange={this.onChange}
-            />
-            <button type="submit" className="btn" style={{ flex: 1 }}>
-              추가
-            </button>
-          </form>
-          <div>
-            {this.state.todoData.map((todo) => (
-              <div style={this.getStyle(todo.checked)} key={todo.id}>
-                <input
-                  type="checkbox"
-                  defaultChecked={todo.checked}
-                  onChange={() => this.onChecked(todo.id)}
-                />
-                {todo.title}
-                <button
-                  style={this.btnStyle}
-                  onClick={() => this.onClickDelte(todo.id)}
-                >
-                  x
-                </button>
-              </div>
-            ))}
-          </div>
+  return (
+    <div className="container">
+      <div className="todoBlock">
+        <div className="title">
+          <h1>Todo List</h1>
+        </div>
+        <form style={{ display: "flex" }} onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="name"
+            style={{ flex: "10", padding: "5px" }}
+            placeholder="할 일을 입력하세요"
+            value={value}
+            onChange={onChange}
+          />
+          <button type="submit" className="btn" style={{ flex: 1 }}>
+            추가
+          </button>
+        </form>
+        <div>
+          {todoData.map((todo) => (
+            <div style={getStyle(todo.checked)} key={todo.id}>
+              <input
+                type="checkbox"
+                defaultChecked={todo.checked}
+                onChange={() => onChecked(todo.id)}
+              />
+              {todo.title}
+              <button style={btnStyle} onClick={() => onClickDelte(todo.id)}>
+                x
+              </button>
+            </div>
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
