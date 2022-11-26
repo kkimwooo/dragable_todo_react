6a0d8ffd357030b234e6b51081ad25c20fddd312
kkimwooo <kkimwooo@gmail.com>
2022-11-26 21:11:16 +0900

    class 형태로 React todo app 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    todoData: [],
+    value: "",
+  };
+
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  getStyle = (checked) => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: checked ? "line-through" : "none",
+    };
+  };
+
+  onClickDelte = (id) => {
+    let newTodoData = this.state.todoData.filter((todo) => todo.id !== id);
+    this.setState({ todoData: newTodoData });
+  };
+
+  onChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodoData = {
+      id: Date.now(),
+      title: this.state.value,
+      checked: false,
+    };
+
+    this.setState({
+      todoData: [...this.state.todoData, newTodoData],
+      value: "",
+    });
+  };
+
+  onChecked = (id) => {
+    let newTodoData = this.state.todoData.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !todo.checked;
+      }
+      return todo;
+    });
+
+    this.setState({ todoData: newTodoData });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>Todo List</h1>
+          </div>
+          <form style={{ display: "flex" }} onSubmit={this.onSubmit}>
+            <input
+              type="text"
+              name="name"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="할 일을 입력하세요"
+              value={this.state.value}
+              onChange={this.onChange}
+            />
+            <button type="submit" className="btn" style={{ flex: 1 }}>
+              추가
+            </button>
+          </form>
+          <div>
+            {this.state.todoData.map((todo) => (
+              <div style={this.getStyle(todo.checked)} key={todo.id}>
+                <input
+                  type="checkbox"
+                  defaultChecked={todo.checked}
+                  onChange={() => this.onChecked(todo.id)}
+                />
+                {todo.title}
+                <button
+                  style={this.btnStyle}
+                  onClick={() => this.onClickDelte(todo.id)}
+                >
+                  x
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
