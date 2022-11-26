014fbc902c0786fa396654e56dcac41eddc024c4
kkimwooo <kkimwooo@gmail.com>
2022-11-27 00:04:47 +0900

    Todo Component 분리

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,8 @@
 import React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import Todo from "./Todo";
 
 export default function TodoList({ todoData, setTodoData }) {
-  const onClickDelte = (id) => {
-    let newTodoData = todoData.filter((todo) => todo.id !== id);
-    setTodoData(newTodoData);
-  };
-
-  const onChecked = (id) => {
-    let newTodoData = todoData.map((todo) => {
-      if (todo.id === id) {
-        todo.checked = !todo.checked;
-      }
-      return todo;
-    });
-
-    setTodoData(newTodoData);
-  };
-
   const dragEnd = (result) => {
     if (!result.destination) return;
 
@@ -46,36 +31,16 @@ export default function TodoList({ todoData, setTodoData }) {
                   index={index}
                 >
                   {(provided, snapshot) => (
-                    <div
+                    <Todo
                       key={todo.id}
-                      {...provided.dragHandleProps}
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      className={` ${
-                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-                    >
-                      <div className="items-center">
-                        <input
-                          type="checkbox"
-                          defaultChecked={todo.checked}
-                          onChange={() => onChecked(todo.id)}
-                        />
-                        <span
-                          className={todo.checked ? "line-through" : undefined}
-                        >
-                          {todo.title}
-                        </span>
-                      </div>
-                      <div className="items-center">
-                        <button
-                          className="px-4 py-2 float-right"
-                          onClick={() => onClickDelte(todo.id)}
-                        >
-                          x
-                        </button>
-                      </div>
-                    </div>
+                      id={todo.id}
+                      title={todo.title}
+                      checked={todo.checked}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
                   )}
                 </Draggable>
               ))}
