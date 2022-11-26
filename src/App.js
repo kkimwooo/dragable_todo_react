ddda6cddb8bed3bb92f165a359ac4f5b357f98eb
kkimwooo <kkimwooo@gmail.com>
2022-11-27 00:29:05 +0900

    All Clear 기능

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ export default function App() {
     setValue("");
   };
 
+  //app.js가 render 될때 onClickDelte 함수가 새로 생성되고 이 함수가 있는 자식 컴포넌트들이 다시 render 됨
+  //그 현상을 막기 위해 onclickDelete 함수를 useCallback으로 감싸서 todoData가 변경될때만 함수가 새로 생성되도록 함
   const onClickDelte = useCallback(
     (id) => {
       let newTodoData = todoData.filter((todo) => todo.id !== id);
@@ -26,11 +28,16 @@ export default function App() {
     [todoData]
   );
 
+  const handleRemoveClick = () => {
+    setTodoData([]);
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow-lg lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>Todo List</h1>
+          <button onClick={handleRemoveClick}>Clear</button>
         </div>
         <InputTodo value={value} setValue={setValue} onSubmit={onSubmit} />
         <TodoList
